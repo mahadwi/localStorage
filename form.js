326e0706bd27edeb3e4c8ef3e00ae9bed9365e4e
mahadwiputra008 <mahadwiputra008@gmail.com>
2023-05-19 10:12:18 +0700

    update localstorage

--- form.js
+++ form.js
@@ -1,48 +1,50 @@
 const storageKey = 'STORAGE_KEY';
 const submitAction = document.getElementById('form-data-user');
 
-function checkForStorage(){
-    return typeof(Storage) !== 'undefined';
+function checkForStorage() {
+    return typeof (Storage) !== 'undefined';
 }
 
-function putUserList(data){
-    if(checkForStorage()){
+function putUserList(data) {
+    if (checkForStorage()) {
         let userData = [];
-        if(localStorage.getItem(storageKey) !== null){
+        if (localStorage.getItem(storageKey) !== null) {
             userData = JSON.parse(localStorage.getItem(storageKey));
         }
-    }
 
-    userData.unshift(data);
-    if(userData.length > 5){
-        userData.pop();
+        userData.unshift(data);
+        if (userData.length > 5) {
+            userData.pop();
+        }
+
+        localStorage.setItem(storageKey, JSON.stringify(userData));
     }
 }
 
-function getUserList(){
-    if(checkForStorage()){
+function getUserList() {
+    if (checkForStorage()) {
         return JSON.parse(localStorage.getItem(storageKey)) || [];
-    }else {
+    } else {
         return [];
     }
 }
 
-function renderUserList(){
+function renderUserList() {
     const userData = getUserList();
     const userList = document.querySelector('#user-list-detail');
 
     userList.innerHTML = '';
-    for(let user of userData){
+    for (let user of userData) {
         let row = document.createElement('tr');
-        row.innerHTML = `<td>${user.nama}</td>`;
-        row.innerHTML = `<td>${user.umur}</td>`;
-        row.innerHTML = `<td>${user.domisili}</td>`;
+        row.innerHTML = '<td>' + user.nama + '</td>';
+        row.innerHTML += '<td>' + user.umur + '</td>';
+        row.innerHTML += '<td>' + user.domisili + '</td>';
 
         userList.appendChild(row);
     }
 }
 
-submitAction.addEventListener('submit', function(event){
+submitAction.addEventListener('submit', function (event) {
     const inputNama = document.getElementById('nama').value;
     const inputUmur = document.getElementById('umur').value;
     const inputDomisili = document.getElementById('domisili').value;
@@ -50,18 +52,18 @@ submitAction.addEventListener('submit', function(event){
         nama: inputNama,
         umur: inputUmur,
         domisili: inputDomisili,
-    }
+    };
 
     putUserList(newUserData);
     renderUserList();
 });
 
-window.addEventListener('load', function(){
-    if(checkForStorage){
-        if(localStorage.getItem(storageKey) !== null){
+window.addEventListener('load', function () {
+    if (checkForStorage) {
+        if (localStorage.getItem(storageKey) !== null) {
             renderUserList();
         }
-    }else {
-        alert('Browser yang anda gunakan tidak mendukung Web Storage');
+    } else {
+        alert('Browser yang Anda gunakan tidak mendukung Web Storage');
     }
-})
+});
